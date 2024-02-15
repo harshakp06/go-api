71ebba255ef68b1ec2c15666ff20f89ddc509d48
Harsha Vardhan <kpharsha@yahoo.com>
2024-02-16 00:33:23 +0530

    add server

--- main.go
+++ main.go
@@ -1,10 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 
+	http.HandleFunc("/", homepage)
 	http.ListenAndServe("localhost:10000", nil)
-	skjdmn
-	sjkdfmxc
+
+}
+
+func homepage(w http.ResponseWriter, r *http.Request) {
+
+	fmt.Fprintf(w, "Welcome to homepage")
+	fmt.Println("Endpoint hit : hoempage")
 }
